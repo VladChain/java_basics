e0ebe6d534f3b2e6c196cf44472c701f2e0b3985
VladChain <vlad.chain@mail.ru>
2020-10-12 09:25:29 +0400

    creating jar-files - cat.jar

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -1,21 +1,19 @@
 
-public class Loader
-{
-    public static void main(String[] args)
-    {
+public class Loader {
+    public static void main(String[] args) {
+
         System.out.println("Amount cats:" + Cat.getCount());
         System.out.println("Add cats: ...");
         System.out.println();
 
 
-        Cat murka = new Cat (1000.0, "Murka", Color.RED);
+        Cat murka = new Cat(1000.0, "Murka", Color.RED);
 
         System.out.println(murka.getWeight());
         System.out.println(murka.getName());
         System.out.println("Murka's color: " + murka.getColor());
 
-        while (murka.getStatus() != "Exploded")
-        {
+        while (murka.getStatus() != "Exploded") {
             murka.feed(10000.0);
 
             System.out.println("Murka weight:" + murka.getWeight());
@@ -26,8 +24,7 @@ public class Loader
         Cat vasya = new Cat();
         System.out.println("Vasya weight:" + vasya.getWeight());
 
-        while (vasya.getStatus() != "Dead")
-        {
+        while (vasya.getStatus() != "Dead") {
             vasya.meow();
 
             System.out.println("Vasya weight:" + vasya.getWeight());
@@ -109,15 +106,14 @@ public class Loader
         Cat rokki = new Cat(1500.0, "Rokki", Color.STRIPED);
 
         Cat atos = new Cat(rokki);
-        System.out.println("Cat Atos: "+atos.getWeight()+", "+atos.getName()+", "+atos.getColor());
+        System.out.println("Cat Atos: " + atos.getWeight() + ", " + atos.getName() + ", " + atos.getColor());
 
         Cat casper = new Cat();
         casper.Copy(rokki);
-        System.out.println("Cat Casper: "+casper.getWeight()+", "+casper.getName()+", "+casper.getColor());
+        System.out.println("Cat Casper: " + casper.getWeight() + ", " + casper.getName() + ", " + casper.getColor());
         System.out.println("Yeah, dude! Good job! Well done! Thanks God & my teacher Ruslan!))");
     }
-        private static Cat getKitten()
-        {
+    private static Cat getKitten() {
         return new Cat(1100.0);
     }
 }
