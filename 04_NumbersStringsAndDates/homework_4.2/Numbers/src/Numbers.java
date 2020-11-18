edcb8dfeefe1dc73a6fec3b9a2f6c6200a6c3285
VladChain <vlad.chain@mail.ru>
2020-11-18 13:49:37 +0400

    homework 4.4 task 2: code total earnings of all friends

---
+++ 04_NumbersStringsAndDates/homework_4.2/Numbers/src/Numbers.java
@@ -0,0 +1,15 @@
+public class Numbers
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Integer data types are: ");
+        System.out.println("byte: from " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+        System.out.println("short: from " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+        System.out.println("int: from " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+        System.out.println("long: from " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+        System.out.println();
+        System.out.println("Floating point types are: ");
+        System.out.println("float: from " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+        System.out.println("double: from " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+    }
+}
--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,27 @@
+import java.util.stream.IntStream;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    System.out.println(text);
-  }
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 
+        int[] sum = new int [1 + 2 + 3];
+        int k = -1;
+
+        StringBuffer total = new StringBuffer();
+        for (int i = 0; i < text.length(); i++) {
+            char wage = text.charAt(i);
+            if (wage > 47 && wage < 58) {
+                total.append(wage);
+                if (text.charAt(i + 1) == ' ') {
+                   sum [++k] = Integer.parseInt(total.toString());
+                    total = new StringBuffer();
+                }
+            }
+        }
+        IntStream intStream = IntStream.of(sum);
+        int totalSum = intStream.sum();
+        System.out.println(totalSum);
+    }
 }
