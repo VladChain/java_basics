b5c650e992936627d3455960e51203ca3b0f9835
VladChain <vlad.chain@mail.ru>
2020-11-28 12:19:34 +0400

    homework 4.5 task 1: total earnings in some examples

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите текст: ");
+//        Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей
+//        Коля заработал 50000 рублей, Федя - 45800 рубля, а Саша - 23000 рублей
+//        Толя заработал 35000 рублей, а Лера - 45000 рублей
+//        Никто ничего не заработал
+        String text = scanner.nextLine();
+        System.out.println(calculateSalarySum(text));
     }
+    public static int calculateSalarySum(String text) {
+        int sum = 0;
 
-    public static int calculateSalarySum(String text) { return -1; }
+        String[] digitArray = text.split(",");
+        for (int i = 0; i < digitArray.length; i++) {
+            if (digitArray.length == 1) {
+                return sum;
+            } else {
+                sum += Integer.parseInt(digitArray[i].replaceAll("[^0-9]", ""));
+            }
+        }
+//        Короткий вариант цикла:
+//        for (String digit : digitArray) {
+//            sum += Integer.parseInt(digit.replaceAll("[^0-9]", ""));
+//        }
+        return sum;
+    }
 }
