2d1911e4afe7c040f3fda3d75e19917e1e40f43c
VladChain <vlad.chain@mail.ru>
2020-12-02 12:48:38 +0400

    homework 4.5 task 2: splitting text by words in a column

--- 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println(splitTextInToWords(text));
+    }
 
-  }
+    public static String splitTextInToWords(String text) {
 
-  public static String splitTextInToWords(String text) {
-    //TODO реализуйте метод
-    return "";
-  }
-
+        text = text.replaceAll("[\\d,.;]", "")
+                .replaceAll("-", " ");
+        String[] output = text.split("\\s+");
+        StringBuilder words = new StringBuilder();
+        for (int i = 0; i < output.length; i++) {
+            if (i != output.length - 1) {
+                words.append(output[i]).append("\r\n");
+            } else
+                words.append(output[i]);
+        }
+        return words.toString();
+    }
 }
+
+
+
