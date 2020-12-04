66b7fbadab0632dad208b07036889bef439bdddc
VladChain <vlad.chain@mail.ru>
2020-12-04 15:00:56 +0400

    homework 4.5 task 3: formatting full name using RegExp

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,18 +1,41 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+//        System.out.println("Введите ФИО: ");
+//    Иванов Иван Иванович
+//    Салтыков-Щедрин Михаил Евграфович
+//    Иван Иван Иванович вв2ввв
+//    1111 2222 3333
+//    Иван
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            Pattern p = Pattern.compile(".+");
+            Matcher m = p.matcher(input);
+            if (m.find()) {
+                input = m.group();
+            }
+            if (!input.isEmpty() && !input.equals("0") &&
+                    input.matches("[а-яА-Я-]+\\s[а-яА-Я]+\\s[а-яА-я]+")) {
+                String title = "Фамилия: Имя: Отчество: ";
+                String[] date = title.split("\\s+");
+                String[] fullName = input.split("\\s+");
+                StringBuilder words = new StringBuilder();
+                for (int i = 0; i < fullName.length; i++) {
+                    words.append(date[i]).append(" ")
+                            .append(fullName[i]).append("\n");
+                }
+                System.out.println(words.toString());
+            } else {
+                System.out.println("Введенная строка не является ФИО");
+            }
+            break;
+        }
     }
-  }
-
 }
