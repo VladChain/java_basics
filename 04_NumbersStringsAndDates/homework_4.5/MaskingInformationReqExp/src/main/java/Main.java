781c72bfd3e89930869b820dc5f0ed1f3a101a23
VladChain <vlad.chain@mail.ru>
2020-12-07 09:55:33 +0400

    homework 4.5 add task: masking information using RegExp

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -1,12 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println(searchAndReplaceDiamonds(text, "***"));
 
+//        Номер кредитной карты <4008 1234 5678> 8912
+//        Пин код <6160>
+//        Номер паспорта <45 08 361>513
+//        Номер паспорта <<45 08 36513
+//        Номер паспорта 45 08 36513
+//        Номер кредитной карты <4008> 1234 <5678> 8912
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+        // Вариант 1:
+        text = text.replaceAll("<[\\d\\s]+>", placeholder);
         return text;
     }
-
 }
+        // Вариант 2:
+//        if (text.matches(".+\\<.+\\>.+\\<.+\\>.+")) {
+//            text = text.replaceAll("\\<\\d+\\>", placeholder);
+//        }
+//        if (text.matches(".+\\<.+\\>.+")) {
+//            text = text.replaceAll("\\<.+\\>", placeholder);
+//        }
+//        if (text.matches(".+\\<.+\\>")) {
+//            text = text.replaceAll("\\<.+\\>", placeholder);
+//        }
