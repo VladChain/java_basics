832177126a838ebf6ea243af7b63b012888be6f5
VladChain <vlad.chain@mail.ru>
2020-12-21 21:57:14 +0300

    homework 5.1 task 2: hospital

--- 05_ArraysAndCollections/homework_5.1/Hospital/src/main/java/Hospital.java
+++ 05_ArraysAndCollections/homework_5.1/Hospital/src/main/java/Hospital.java
@@ -2,21 +2,41 @@ public class Hospital {
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
-        //TODO: напишите метод генерации массива температур пациентов
+        float[] countOfPatient = new float[patientsCount];
+        for (int i = 0; i < countOfPatient.length; i++)
+            countOfPatient[i] = Math.round(32.0 + (Math.random() * (40.0 - 32.0)));
+        for (float tempOfPatient : countOfPatient)
+            System.out.print(tempOfPatient + " ");
 
-        return new float[0];
+        return countOfPatient;
     }
 
     public static String getReport(float[] temperatureData) {
-        /*
-        TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
-            а также температуры всех пациентов.
-        */
+
+        StringBuilder tempOfPatient = new StringBuilder();
+        for (float allTemp : temperatureData)
+            tempOfPatient.append(allTemp).append(" ");
+        String allTemperature = tempOfPatient.toString().trim();
+
+        float temp = 0;
+        StringBuilder midTemp = new StringBuilder();
+        for (float allTemp : temperatureData)
+            temp += allTemp;
+        temp = temp / 30;
+        String result = String.format("%.1f", temp);
+        midTemp.append(result);
+
+        int patients = 0;
+        StringBuilder healthPatients = new StringBuilder();
+        for (float normalTemp : temperatureData)
+            if (normalTemp >= 36 && normalTemp <= 37)
+                patients++;
+        healthPatients.append(patients);
 
         String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
+                "Температуры пациентов: " + allTemperature +
+                        "\nСредняя температура: " + midTemp +
+                        "\nКоличество здоровых: " + healthPatients;
 
         return report;
     }
