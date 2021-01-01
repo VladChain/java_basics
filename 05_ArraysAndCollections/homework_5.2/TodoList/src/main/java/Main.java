72afb06215e80b2df9ad31edde193c1f52a5342d
VladChain <vlad.chain@mail.ru>
2021-01-01 18:03:31 +0300

    homework 5.2 task: todos' list

--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,46 @@
+import java.util.*;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+        System.out.println("Выберите команду из списка, что вы хотите сделать: " +
+                "\n LIST - вывести список дел " +
+                "\n ADD - добавить дело в список " +
+                "\n EDIT - отредактировать список дел " +
+                "\n DELETE - удалить дело из списка");
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            switch (sc.nextLine()) {
+                case "LIST":
+                    System.out.println(todoList.getTodos());
+                    continue;
+                case "ADD":
+                    if (sc.hasNextInt()) {
+                        int addNumTodo = sc.nextInt();
+                        String newTodo = sc.nextLine();
+                        todoList.add(addNumTodo, newTodo);
+                    } else {
+                        String newTodo = sc.nextLine();
+                        todoList.add(newTodo);
+                    }
+                    continue;
+                case "EDIT":
+                    int numTodo = sc.nextInt();
+                    String newTodo = sc.nextLine();
+                    todoList.edit(newTodo, numTodo);
+                    continue;
+                case "DELETE" :
+                    int delTodo = sc.nextInt();
+                    todoList.delete(delTodo);
+                    continue;
+                default:
+                    System.out.println("Введите правильно команду!");
+                    break;
+            }
+            sc.close();
+        }
     }
 }
--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
@@ -2,28 +2,46 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    private ArrayList<String> todoList = new ArrayList<>();
+
     public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+        todoList.add(todo);
+        System.out.println(todo);
     }
 
     public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+        if (index < todoList.size()) {
+            todoList.add(index, todo);
+            System.out.println(todo);
+        }
+        if (index > todoList.size()) {
+            System.out.println("Размер массива: " + todoList.size() + ", дело добавлено в " +
+                    "конец списка");
+            todoList.add(todo);
+            System.out.println(todo);
+        }
     }
 
     public void edit(String todo, int index) {
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
+        if (index < todoList.size()) {
+            todoList.add(index, todo);
+            System.out.println(todo);
+        }
+        if (index < todoList.size()) {
+            todoList.remove(index + 1);
+            System.out.println(todo);
+        }
     }
 
     public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
+        if (index < todoList.size()) {
+            todoList.remove(index);
+        } else {
+            System.out.println("Дело с таким номером не существует");
+        }
     }
 
     public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
+        return todoList;
     }
-
 }
