e3b1e91499d4cc215e8aad72f725029ce0af6e58
VladChain <vlad.chain@mail.ru>
2021-02-13 15:07:33 +0300

    homework 7.1 task: learn lambda expressions

---
+++ 07_AdvancedOOPFeatures_with_tests_29_01_21/07_AdvancedOOPFeatures/homework_7.1/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static final String STAFF_TXT = "data/staff.txt";
+
+    public static void main(String[] args) {
+
+        List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
+        sortBySalaryAndAlphabet(staff);
+    }
+
+    public static void sortBySalaryAndAlphabet(List<Employee> staff) {
+        //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+        Collections.sort(staff, Comparator.comparingInt(Employee::getSalary)
+                .thenComparing(Employee::getName));
+        for (Employee employee : staff) {
+            System.out.println(employee);
+        }
+    }
+}
