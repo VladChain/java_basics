7a5955c50f0e1e2bf1f9a2d70cf45a52fcd70457
VladChain <vlad.chain@mail.ru>
2021-02-17 21:57:08 +0300

    homework 7.2 task: get experience with the Steam API (class Employee)

---
+++ 07_AdvancedOOPFeatures_with_tests_29_01_21/07_AdvancedOOPFeatures/homework_7.2/Employees/src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+import static java.lang.System.out;
+
+public class Main {
+
+    private static final String STAFF_TXT = "data/staff.txt";
+
+    public static void main(String[] args) {
+        List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
+        Employee employeeMaxSalary = findEmployeeWithHighestSalary(staff, 2017);
+        out.println(employeeMaxSalary);
+    }
+
+    public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
+        //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
+        // кто пришёл в году, указанном в переменной year
+        return  staff.stream().filter(e -> e.getWorkStart().getYear() == year - 1900)
+                .max(Comparator.comparing(Employee::getSalary))
+                .get();
+    }
+}
