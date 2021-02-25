cc8a0b7b8feb6022d7d51ae9c9016447fb531c72
VladChain <vlad.chain@mail.ru>
2021-02-25 15:30:34 +0300

    homework 8.1 task: using the exception mechanism

---
+++ 08_ExceptionsDebuggingAndTesting_with_tests_20_12_2020/08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -0,0 +1,63 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomerStorage {
+    private final Map<String, Customer> storage;
+
+    public CustomerStorage() {
+        storage = new HashMap<>();
+    }
+
+    public void addCustomer(String data) {
+        final int INDEX_NAME = 0;
+        final int INDEX_SURNAME = 1;
+        final int INDEX_EMAIL = 2;
+        final int INDEX_PHONE = 3;
+
+        String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Wrong format. Correct format: \n"
+                    + "add Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
+
+        String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+
+        Pattern phone = Pattern.compile("\\+[0-9]+");
+        Matcher matcherPhone = phone.matcher(components[INDEX_PHONE]);
+        if (matcherPhone.find()) {
+            components[INDEX_PHONE] = matcherPhone.group();
+        } else {
+            throw new IllegalArgumentException("Wrong format. Correct format: \n"
+                    + "+79215637722");
+        }
+
+        Pattern mail = Pattern.compile("^[a-z]+@.+\\..+$");
+        Matcher matcherMail = mail.matcher(components[INDEX_EMAIL]);
+        if (matcherMail.find()) {
+            components[INDEX_EMAIL] = matcherMail.group();
+        } else {
+            throw new IllegalArgumentException("Wrong format. Correct format: \n"
+                    + "vasily.petrov@gmail.com");
+        }
+
+        storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+    }
+
+    public void listCustomers() {
+        storage.values().forEach(System.out::println);
+    }
+
+    public void removeCustomer(String name) {
+        storage.remove(name);
+    }
+
+    public Customer getCustomer(String name) {
+        return storage.get(name);
+    }
+
+    public int getCount() {
+        return storage.size();
+    }
+}
