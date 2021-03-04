e4d726d944171742c189748f7e319a0550b03daf
VladChain <vlad.chain@mail.ru>
2021-03-04 15:29:33 +0300

    homework 8.2 task: writing tests for classes using the JUnit5.

--- 08_ExceptionsDebuggingAndTesting_with_tests_20_12_2020/08_ExceptionsDebuggingAndTesting/homework_8.2/SPBMetro/src/test/java/RouteCalculatorTests.java
+++ 08_ExceptionsDebuggingAndTesting_with_tests_20_12_2020/08_ExceptionsDebuggingAndTesting/homework_8.2/SPBMetro/src/test/java/RouteCalculatorTests.java
@@ -1,16 +1,19 @@
 import core.Line;
 import core.Station;
-import junit.framework.TestCase;
+import org.junit.jupiter.api.*;
 import java.util.ArrayList;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class RouteCalculatorTests extends TestCase {
+@DisplayName("Тест расчета маршрута")
+public class RouteCalculatorTests {
 
-    List<Station> route = new ArrayList<>();
+    List<Station> route;
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    @BeforeEach
+    void setUp() {
+        route = new ArrayList<>();
 
         Line line1 = new Line(5, "Фрунзенско-Приморская");
         Line line2 = new Line(2, "Московско-Петроградская");
@@ -24,18 +27,53 @@ public class RouteCalculatorTests extends TestCase {
         route.add(new Station("Василеостровская", line3));
     }
 
-    public void testGetShortestRoute() {
-        assertNotNull("Список не пустой", route);
+    @DisplayName("Список не пустой")
+    @Test
+    void testGetShortestRoute() {
+        assertNotNull(route);
     }
 
-    public void testCalculateDuration() {
+    @DisplayName("Время в пути")
+    @Test
+    void testCalculateDuration() {
         double actual = RouteCalculator.calculateDuration(route);
         double expected = 14.5;
         assertEquals(expected, actual);
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    @AfterEach
+    void tearDown() {
     }
 }
+// ==============================================================================================
+//    @Override
+//    protected void setUp() throws Exception {
+//        super.setUp();
+//
+//        Line line1 = new Line(5, "Фрунзенско-Приморская");
+//        Line line2 = new Line(2, "Московско-Петроградская");
+//        Line line3 = new Line(3, "Невско-Василеостровская");
+//
+//        route.add(new Station("Адмиралтейская", line1));
+//        route.add(new Station("Садовая", line1));
+//        route.add(new Station("Сенная площадь", line2));
+//        route.add(new Station("Невский проспект", line2));
+//        route.add(new Station("Гостиный двор", line3));
+//        route.add(new Station("Василеостровская", line3));
+//    }
+//
+//    public void testGetShortestRoute() {
+//        assertNotNull("Список не пустой", route);
+//    }
+//
+//    public void testCalculateDuration() {
+//        double actual = RouteCalculator.calculateDuration(route);
+//        double expected = 14.5;
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Override
+//    protected void tearDown() throws Exception {
+//        super.tearDown();
+//    }
+
