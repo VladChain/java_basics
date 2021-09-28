4dd93a93dfa19d4991fb9ac534bebcb62b2df2b3
VladChain <vlad.chain@mail.ru>
2021-09-28 08:57:23 +0500

    homework 19.2: working with XML file parsers and different principles of their operation

---
+++ 19_Performance/19_Performance/VoteAnalyzer/src/XMLHandler.java
@@ -0,0 +1,53 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+public class XMLHandler extends DefaultHandler {
+
+    private Voter voter;
+    private static SimpleDateFormat birthDayFormat = new SimpleDateFormat("yyyy.MM.dd");
+    private HashMap<Voter, Integer> voterCounts;
+
+    public XMLHandler() {
+        voterCounts = new HashMap<>();
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
+        try {
+
+            if (qName.equals("voter") && voter == null) {
+                Date birthDay = birthDayFormat.parse(attributes.getValue("birthDay"));
+                voter = new Voter(attributes.getValue("name"), birthDay);
+            }
+            else if (qName.equals("visit") && voter != null) {
+                int count = voterCounts.getOrDefault(voter, 0);
+                voterCounts.put(voter, count + 1);
+            }
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) {
+        if (qName.equals("voter")) {
+            voter = null;
+        }
+    }
+
+    public void printDuplicatedVoters() {
+        System.out.println("Duplicated voters: ");
+        for (Voter voter : voterCounts.keySet() ) {
+
+            int count = voterCounts.get(voter);
+            if (count > 1) {
+                System.out.println("\t" + voter.toString() + " - " + count);
+            }
+        }
+    }
+}
